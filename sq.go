19a13a979bffe03b98ee13e778fb8eefdc44cba1
Henry Qin <root@hq6.me>
2024-06-08 17:23:42 -0700

    Add path to discovered commands.

--- sq.go
+++ sq.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/square/exit"
 	"github.com/square/exoskeleton"
@@ -19,9 +20,18 @@ func main() {
     dir = filepath.Dir(dir)
   }
 
+  home := os.Getenv("HOME")
+
 	cli, _ := exoskeleton.New(paths,
-      exoskeleton.WithModuleMetadataFilename(".sq-module"))
+      exoskeleton.WithModuleMetadataFilename(".sq-module"),
+      exoskeleton.WithMenuHeadingFor(func(m exoskeleton.Module, c exoskeleton.Command) string {
+        return "COMMANDS IN " + strings.Replace(c.(exposeDiscoveredIn).DiscoveredIn(), home, "~", 1)
+      }))
 	cmd, args, _ := cli.Identify(os.Args[1:])
 	err := cmd.Exec(cli, args, os.Environ())
 	os.Exit(exit.FromError(err))
+}
+
+type exposeDiscoveredIn interface {
+  DiscoveredIn() string
 }
