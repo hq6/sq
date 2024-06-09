6e7d9acebf2780d57af5f7869fe02081fe813587
Henry Qin <root@hq6.me>
2024-06-08 17:27:15 -0700

    Run go mod tidy and go fmt.

--- sq.go
+++ sq.go
@@ -11,27 +11,27 @@ import (
 
 func main() {
 	paths := []string{}
-  dir, _ := os.Getwd()
-  for dir != "/" {
-    path := dir + "/sqbin"
-    if _, err := os.Stat(path); err == nil {
-      paths = append(paths, path)
-    }
-    dir = filepath.Dir(dir)
-  }
+	dir, _ := os.Getwd()
+	for dir != "/" {
+		path := dir + "/sqbin"
+		if _, err := os.Stat(path); err == nil {
+			paths = append(paths, path)
+		}
+		dir = filepath.Dir(dir)
+	}
 
-  home := os.Getenv("HOME")
+	home := os.Getenv("HOME")
 
 	cli, _ := exoskeleton.New(paths,
-      exoskeleton.WithModuleMetadataFilename(".sq-module"),
-      exoskeleton.WithMenuHeadingFor(func(m exoskeleton.Module, c exoskeleton.Command) string {
-        return "COMMANDS IN " + strings.Replace(c.(exposeDiscoveredIn).DiscoveredIn(), home, "~", 1)
-      }))
+		exoskeleton.WithModuleMetadataFilename(".sq-module"),
+		exoskeleton.WithMenuHeadingFor(func(m exoskeleton.Module, c exoskeleton.Command) string {
+			return "COMMANDS IN " + strings.Replace(c.(exposeDiscoveredIn).DiscoveredIn(), home, "~", 1)
+		}))
 	cmd, args, _ := cli.Identify(os.Args[1:])
 	err := cmd.Exec(cli, args, os.Environ())
 	os.Exit(exit.FromError(err))
 }
 
 type exposeDiscoveredIn interface {
-  DiscoveredIn() string
+	DiscoveredIn() string
 }
